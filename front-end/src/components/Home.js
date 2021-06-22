ab27f1a8f1c8549e0c08d26c19ce540cef8dfa26
phoebe <phoebe@localhost.localdomain>
2021-06-21 19:13:36 -0600

    change floresse homepage

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -3,6 +3,11 @@ import 'semantic-ui-css/semantic.min.css';
 import { Header, Icon, Modal, Button, } from "semantic-ui-react";
 import '../App.css';
 
+import background_video from '../videos/crop-test.mp4';
+import title_image from '../images/Title.png';
+import andy_pic from '../images/ANDY.JPEG';
+import scott_pic from '../images/SCOTT.jpg';
+;
 class Home extends Component {
     constructor(props) {
 		super(props);
@@ -30,46 +35,73 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <Modal open={this.state.modalOpen} onClose={this.handleClose} id="modalthing">
-                    <Header id="modalheader" content="PeePeePooPooPopUp"/>
-                    <Modal.Content id="content1">
-                        <center>
-                            <iframe title="drive-listen" id="driveListen" width="800" height="500" src="https://app.drive-listen.com"/>
-                        </center>
-                    </Modal.Content>
-                    <Modal.Actions id="bottomarea">
-                        <Button color="red" onClick={this.handleClose} inverted>
-                            <Icon name="cancel"/> Close
-                        </Button>
-                    </Modal.Actions>
-                </Modal>
-                <p></p>
-                <header className="App-header">
-                    <h1 id="welcome">
-                        W E L C O M E
-                    </h1>
-                    <img id="SCPthing" src="images/SCP_Foundation.png" className="App-logo" alt="spinning-logo" />
-                    <p>
-                        Cindy is a poo poo face
-                    </p>
-                    <p>
-                        Sphynx of Black Quartz, Judge My Vow
-                    </p>
-                    <a
-                    className="App-link"
-                    href="https://duckduckgo.com"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >DuckDuckGo</a>
-
-                    <div class="row">
-                        <div class="column">
-                            <button id="peepoobutton" onClick={(event) => this.openModal(event)}>
-                                Useless Button
-                            </button>
-                        </div>
+                <div className="title-section">
+                    <img src={title_image}/>
+                </div>
+                
+                <div>
+                    <div className="background-video-container">
+                        <video className="background-video" src={background_video} autoPlay="true" loop="true" muted="true"/>
                     </div>
-                </header>
+                </div>
+                
+                <div className="video-overlay">
+                    <h1 className="overlay-title">
+                        What we do
+                    </h1>
+
+                    <p className="overlay-content">
+						Floresse is a locally run organization based in Colorado Springs and 
+                        Provo Utah. Our team is composed of passionate Web Developers and Digital 
+                        Artists who love to make websites for people. We would like to get to know our clients so that we can make the highest 
+                        quality website possible to ensure our customer satisfaction.
+					</p>
+                </div>
+
+                
+
+                <div className="content-container">
+                    <header className="App-header">
+                        {/*<img id="SCPthing" src="images/SCP_Foundation.png" className="App-logo" alt="spinning-logo" />*/}
+                        <p></p>
+
+                        <div className="profiles">
+                            <h1 className="section-headers">
+                                About the Team
+                            </h1>
+
+                            <div className="andy-container">
+                                <div className="profile-picture">
+                                    <img id="andy-pic" src={andy_pic}/>
+                                    <div className="profile-description">
+                                        <h1 id="name">Andy Wilson</h1>
+                                        <p id="profile-description">
+                                            Andy is a graduate from Colorado State University in Fort 
+                                            Collins. With a Bachelor's Degree in Computer Science, Andy 
+                                            uses is skills to be a freelance software developer and web 
+                                            application developer for Floresse.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="scott-container">
+                                <div className="profile-picture">
+                                    <img id="scott-pic" src={scott_pic}/>
+                                    <div className="profile-description">
+                                        <h1 id="name">Scott Draper</h1>
+                                        <p id="profile-description">
+                                            Scott is a highly experienced freelance software developer
+                                            who specializes in web application development and data
+                                            science. The experience he has under his belt has enabled 
+                                            him to be an effective web designer.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </header>
+                </div>
             </div>
         );
     }
--- front-end/src/components/TopMenu.js
+++ front-end/src/components/TopMenu.js
@@ -5,37 +5,25 @@ import { Link } from "react-router-dom";
 export default class TopMenu extends Component {
     render() {
         return (
-            <Menu style={{marginTop: "0px", backgroundColor: "#61dafb"}}>
-                <Menu.Item>
-                    <Link to={{ pathname: "/"}}>
-                        <button id="TopBarButton">
-                            Home
-                        </button>
-                    </Link>
-                </Menu.Item>
+            <div className="top-bar">
+                <Link to={{ pathname: "/"}}>
+                    <button id="TopBarButton">
+                        Home
+                    </button>
+                </Link>
 
-                <Menu.Item>
-                    <Link to={{ pathname: "/Explore"}}>
-                        <button id="TopBarButton">
-                            Explore
-                        </button>
-                    </Link>
-                </Menu.Item>
+                <Link to={{ pathname: "/Explore"}}>
+                    <button id="TopBarButton">
+                        Explore
+                    </button>
+                </Link>
 
-                <Menu.Item>
-                    <Link to={{ pathname: "/About"}}>
-                        <button id="TopBarButton">
-                            About
-                        </button>
-                    </Link>
-                </Menu.Item>
-
-                <Menu.Item position="right">
-                    <h1>
-                        S Q U I N C H N E T
-                    </h1>
-                </Menu.Item>
-            </Menu>
+                <Link to={{ pathname: "/About"}}>
+                    <button id="TopBarButton">
+                        About
+                    </button>
+                </Link>
+            </div>
         );
     }
 }
